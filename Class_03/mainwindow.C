a8b1f85f4473adf154595da6820a9cd842de0e01
Guilherme Pedroso <guilhermepedrosogg@gmail.com>
2022-08-21 00:33:30 -0300

    Project class 3

--- Class_03/mainwindow.C
+++ Class_03/mainwindow.C
@@ -16,6 +16,16 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
+ /*
   QMessageBox::about(this, "Github", "https://github.com/guiipedroso/developing_qt");
+
+  QMessageBox::aboutQt(this);
+
+  QMessageBox::critical(this,"Github", "not found");
+
+  QMessageBox::information(this,"Github","follow https://github.com/guiipedroso/developing_qt");
+*/
+   QMessageBox::question(this,"Github","Do you want follow https://github.com/guiipedroso/developing_qt");
+
 }
 
