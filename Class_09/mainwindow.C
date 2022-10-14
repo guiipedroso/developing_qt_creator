50f1be968025a64aa854ba4c0aaf222b9dd4f306
Guilherme Pedroso <guilhermepedrosogg@gmail.com>
2022-10-14 16:07:39 -0300

    Insertion of images

--- Class_09/mainwindow.C
+++ Class_09/mainwindow.C
@@ -1,15 +1,24 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include "QPixmap"
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QPixmap logo("/home/pedroso/Downloads/Casa_Baden.jpg");
+    ui->label->setPixmap(logo.scaled(580,220,Qt::KeepAspectRatio));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QPixmap imagem("/home/pedroso/Downloads/hello.jpeg");
+    ui->label->setPixmap(imagem.scaled(580,220,Qt::KeepAspectRatio));
 }
 
