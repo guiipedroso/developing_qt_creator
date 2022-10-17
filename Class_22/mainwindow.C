f302d28923cad1c0168350fc82ae223461c8773d
Guilherme Pedroso <guilhermepedrosogg@gmail.com>
2022-10-17 00:17:55 -0300

    QTime

---
+++ Class_22/mainwindow.C
@@ -0,0 +1,28 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDateTime>
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    tempo = new QTimer(this);
+    connect(tempo, SIGNAL(timeout()), this, SLOT(atualizaRelogio()));
+    tempo->start(1000);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void
+MainWindow::atualizaRelogio()
+  {
+  QTime tempoAtual = QTime::currentTime();
+  QString tempoTexto = tempoAtual.toString("hh:mm:ss");
+  qDebug() << tempoAtual;
+  ui->label->setText(tempoTexto);
+  }
