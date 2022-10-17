93e581ecb55d0b13a6537c072a0377c805724a86
Guilherme Pedroso <guilhermepedrosogg@gmail.com>
2022-10-16 23:38:37 -0300

    QTimer

---
+++ Class_21/mainwindow.C
@@ -0,0 +1,26 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+static int cont = 0;
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    tempo=new QTimer(this);
+    connect(tempo, SIGNAL(timeout()),this,SLOT(minhaFuncao()));
+    tempo->start(1000);
+
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::minhaFuncao()
+  {
+  cont++;
+  qDebug() << "Timer: "+ QString::number(cont)+"s";
+  }
