9a099b630f502c585bb583faaf477cfd4d2d2804
Guilherme Pedroso <guilhermepedrosogg@gmail.com>
2023-08-01 21:52:33 -0300

    Text Editor

---
+++ Projects/Text_Editor/editordetextos.C
@@ -0,0 +1,184 @@
+#include "editordetextos.h"
+#include "ui_editordetextos.h"
+
+EditorDeTextos::EditorDeTextos(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::EditorDeTextos)
+{
+    ui->setupUi(this);
+    this->setStyleSheet("background: white; color:black;");
+    this->setCentralWidget( ui->textEdit);
+}
+
+EditorDeTextos::~EditorDeTextos()
+{
+    delete ui;
+}
+
+
+void EditorDeTextos::on_actionNovo_triggered()
+{
+    localfilename = "";
+    ui->textEdit->clear();
+    ui->textEdit->setFocus();
+}
+
+
+void EditorDeTextos::on_actionAbrir_triggered()
+{
+    QString filename = QFileDialog::getOpenFileName(this, "Abrir", QDir::homePath(), all_files);
+    QFile file( filename );
+    localfilename = filename;
+
+    if(file.exists()) // se ele existir
+      {
+        if(!file.open( QFile::ReadOnly | QFile::Text))
+          {
+            QMessageBox::warning(this, "Erro ao abrir arquivo", "Arquivo não pode ser aberto");
+                return;
+          }
+
+        QTextStream enter_file( &file);
+        QString text = enter_file.readAll();
+        ui->textEdit->setText(text);
+      }
+
+    file.close();
+}
+
+
+void EditorDeTextos::on_actionSalvar_triggered()
+{
+    QFile file (localfilename);
+
+    if(file.exists())
+      {
+      if(!file.open( QFile::WriteOnly | QFile::Text))
+        {
+        QMessageBox::warning(this, "Erro", "Arquivo não pode ser salvo");
+        return;
+        }
+
+      QTextStream out_file( &file);
+      QString text = ui->textEdit->toPlainText();
+      out_file << text << '\n';
+
+      file.flush(); // limpar todo conteudo
+      file.close();
+      }
+    else
+      {
+      on_actionSalvar_como_triggered(); //pega funcao do salvar como
+      }
+}
+
+
+void EditorDeTextos::on_actionSalvar_como_triggered()
+{
+    QString filename = QFileDialog::getSaveFileName(this, "Salvar como", QDir::homePath(), all_files);
+    localfilename = filename;
+
+    if( filename == "" )
+    {
+        return;
+    }
+
+    QFile file (filename);
+
+    if(!file.open( QFile::WriteOnly | QFile::Text))
+    {
+        if(file.exists()) // se ele existir
+        {
+                QMessageBox::warning(this, "Erro ao salvar o arquivo", "Arquivo não pode ser salvo");
+        }
+
+        return;
+    }
+
+    QTextStream out_file( &file);
+    QString text = ui->textEdit->toPlainText();
+    out_file << text << '\n';
+
+    file.flush(); // limpar todo conteudo
+    file.close();
+}
+
+
+void EditorDeTextos::on_actionCopiar_triggered()
+{
+    ui->textEdit->copy();
+}
+
+
+void EditorDeTextos::on_actionColar_triggered()
+{
+    ui->textEdit->paste();
+}
+
+
+void EditorDeTextos::on_actionRecortar_triggered()
+{
+    ui->textEdit->cut();
+}
+
+
+void EditorDeTextos::on_actionRefazer_triggered()
+{
+    ui->textEdit->redo();
+}
+
+
+void EditorDeTextos::on_actionDesfazer_triggered()
+{
+    ui->textEdit->undo();
+}
+
+
+void EditorDeTextos::on_actionSair_triggered()
+{
+    close();
+}
+
+
+void EditorDeTextos::on_actionCor_triggered()
+{
+    QColor color = QColorDialog::getColor(Qt::black, this, "Escolha uma cor ");
+    if(color.isValid())
+      {
+        ui->textEdit->setTextColor(color);
+      }
+}
+
+
+void EditorDeTextos::on_actionBackground_triggered()
+{
+    QColor color = QColorDialog::getColor(Qt::black, this, "Escolha uma cor ");
+    if(color.isValid())
+    {
+      ui->textEdit->setTextBackgroundColor(color);
+    }
+}
+
+
+void EditorDeTextos::on_actionFonte_triggered()
+{
+    bool ok;
+    QFont font = QFontDialog::getFont(&ok, this);
+
+    if(ok)
+      {
+      ui->textEdit->setFont(font);
+      }
+    else
+      {
+      return;
+      }
+}
+
+
+void EditorDeTextos::on_actionSobre_Devs_triggered()
+{
+      QString link("https://github.com/guiipedroso");
+      QUrl url(link);
+}
+
---
+++ Projects/Text_Editor/editordetextos.h
@@ -0,0 +1,61 @@
+#ifndef EDITORDETEXTOS_H
+#define EDITORDETEXTOS_H
+
+#include <QMainWindow>
+#include <QFile>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QMessageBox>
+#include <QFontDialog>
+#include <QFont>
+#include <QColor>
+#include <QColorDialog>
+#include <QUrl>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class EditorDeTextos; }
+QT_END_NAMESPACE
+
+class EditorDeTextos : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    EditorDeTextos(QWidget *parent = nullptr);
+    QString localfilename;
+    QString all_files = " Arquivos de texto (*.txt);; Arquivos Markdown (*.md)";
+    ~EditorDeTextos();
+
+private slots:
+    void on_actionNovo_triggered();
+
+    void on_actionAbrir_triggered();
+
+    void on_actionSalvar_triggered();
+
+    void on_actionSalvar_como_triggered();
+
+    void on_actionCopiar_triggered();
+
+    void on_actionColar_triggered();
+
+    void on_actionRecortar_triggered();
+
+    void on_actionRefazer_triggered();
+
+    void on_actionDesfazer_triggered();
+
+    void on_actionSair_triggered();
+
+    void on_actionCor_triggered();
+
+    void on_actionBackground_triggered();
+
+    void on_actionFonte_triggered();
+
+    void on_actionSobre_Devs_triggered();
+
+private:
+    Ui::EditorDeTextos *ui;
+};
+#endif // EDITORDETEXTOS_H
